2892a0df3a4dc53482d41a4a7eec1c4cf7a22bdc
Jon Wendt <jpwendt@asu.edu>
2012-07-30 18:07:20 -0700

    Added time remaining on auctions view

--- app/helpers/auctions_helper.rb
+++ app/helpers/auctions_helper.rb
@@ -1,6 +1,24 @@
 module AuctionsHelper
   
   def get_time_remaining(auction)
-    Time.parse(auction.item[:get_item_response][:item][:listing_details][:end_time]).localtime
+    # Find the time remaining in seconds
+    @time_left = (Time.parse(auction.item[:get_item_response][:item][:listing_details][:end_time]).localtime - Time.now).to_int
+    if @time_left < 0
+      return "0 seconds"
+    end
+    # Format the time
+    mins = @time_left / 60
+    hours = mins / 60
+    days = hours / 24
+    
+    if days > 0
+      "#{days}d #{hours % 24}h"
+    elsif hours > 0
+      "#{hours}h #{mins % 60}h"
+    elsif mins > 0
+      "#{mins}m #{@time_left % 24}s"
+    elsif secs >= 0
+      "#{@time_left} seconds"
+    end
   end
 end

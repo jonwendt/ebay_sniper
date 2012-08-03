026cbd52fa8c42c3a8117efb0ffa9846fc389f30
Jon Wendt <jpwendt@asu.edu>
2012-08-03 16:52:44 -0700

    Improved validations for auctions and users, reinforced MVC in Auctions.

--- app/controllers/auctions_controller.rb
+++ app/controllers/auctions_controller.rb
@@ -7,7 +7,7 @@ class AuctionsController < ApplicationController
     if params[:status] == "All"
       @auctions = current_user.auctions
     else
-      @auctions = sort_auctions(params[:status])
+      @auctions = Auction.sort_auctions(params[:status], current_user)
     end
 
     respond_to do |format|
@@ -41,7 +41,7 @@ class AuctionsController < ApplicationController
   # GET /auctions/1/edit
   def edit
     @auction = Auction.find(params[:id])
-    @auction = update_auction(@auction)
+    @auction.update_auction @auction
     @picture_id = params[:pic].to_i
     
     respond_to do |format|
@@ -54,56 +54,16 @@ class AuctionsController < ApplicationController
   # POST /auctions.json
   def create
     @auction = Auction.new(params[:auction])
-    @auction.user = current_user
-    
-    # Parse the eBay item URL for the item's ID, then get the item's info
-    @auction.item_id = self.parse_url_for_item_id(@auction.item_id)
-    @auction.item = EbayAction.new.get_item(@auction.item_id, "")
-    
-    # If the auction is real
-    if @auction.item[:get_item_response][:ack] == "Success"
-      find_status(@auction)
-    
-      # Load the listing's pictures. If the item's seller didn't include a picture, load ebay's
-      # default picture. Else, check if there are multiple pictures. If true, push them all into the
-      # pictures array. If there's only one picture, then push that in.
-      if @auction.item[:get_item_response][:item][:picture_details][:photo_display] == "None"
-        @auction.picture.push "http://p.ebaystatic.com/aw/pics/nextGenVit/imgNoImg.gif"
-      else
-        @pictures = @auction.item[:get_item_response][:item][:picture_details][:picture_url]
-        if @pictures.respond_to?(:each)
-          @pictures.each do |picture|
-            @auction.picture.push picture.to_s
-          end
-        else
-          @auction.picture.push @pictures.to_s
-        end
-      end
-    
-      # If the auction is still going, enqueue an AuctionBidder worker to bid on the auction
-      if @auction.auction_status == "Active"
-        Resque.enqueue_at(self.get_enqueue_time(@auction.item[:get_item_response][:item][:listing_details][:end_time],
-                          @auction.lead_time).seconds.from_now, AuctionBidder, @auction.id)
-      end
-      
-      # If the user wants updates on the auction, but has not provided a phone number with which to contact them, notify the user.
-      if @auction.user_notification != "Do not notify" && current_user.phone_number == ""
-        @phone_notice = " If you wish to receive notifications on your phone, please add your phone number on the \"Edit account\" page."
-      end
+    @auction.prepare @auction, current_user
 
-      respond_to do |format|
-        if @auction.save
-          format.html { redirect_to edit_auction_path(@auction.id), notice: "Auction was successfully created.#{@phone_notice}" }
-          format.json { render json: @auction, status: :created, location: @auction }
-        else
-          format.html { redirect_to new_auction_path, notice: "The auction's item ID was invalid." }
-          format.json { render json: @auction.errors, status: :unprocessable_entity }
-        end
+    respond_to do |format|
+      if @auction.save
+        format.html { redirect_to edit_auction_path(@auction.id), notice: "Auction was successfully created." }
+        format.json { render json: @auction, status: :created, location: @auction }
+      else
+        format.html { render action: "new" }#redirect_to new_auction_path, notice: @auction.errors }#"The auction's item ID was invalid." }
+        format.json { render json: @auction.errors, status: :unprocessable_entity }
       end
-      
-    # The auction does not exist. Redirect to new auction page.
-    else
-      redirect_to new_auction_path, notice: "The auction's item ID was invalid."
     end
   end
 
@@ -134,77 +94,5 @@ class AuctionsController < ApplicationController
       format.html { redirect_to auctions_url + "?status=All" }
       format.json { head :no_content }
     end
-  end
-
-  def update_auction(auction)
-    if auction.auction_status == "Active"
-      # Sadly, this doesn't work because of the nested hashes.
-      # auction.item.merge! EbayAction.new.get_item(auction.item_id, "timeleft,bidcount,currentprice,userid")
-      
-      # Figure out how to update while only grabbing values needed
-      @new_auction = EbayAction.new.get_item(auction.item_id, "")
-      auction.item = auction.item.merge @new_auction
-      find_status(auction)
-    end
-    auction
-  end
-  
-  # Finds the current status of the auction (active, won, lost, etc)
-  def find_status(auction)
-    # If the auction is over, check if we won or lost
-    if auction.item[:get_item_response][:item][:time_left] == "PT0S"
-      # Change current_user.name to wherever the user's ebay username is stored
-      begin
-        if auction.item[:get_item_response][:item][:selling_status][:high_bidder][:user_id] == "testuser_jpwendt2"
-          auction.auction_status = "Won"
-        else
-          auction.auction_status = "Lost"
-        end
-      rescue
-        auction.auction_status = "Lost"
-      end
-    else
-      auction.auction_status = "Active"
-    end
-  end
-  
-  # Returns the appropriate auctions based on the user's selected auction status preference.
-  def sort_auctions(status)
-    @auctions = []
-    # If the status == "Ended" return all Won, Lost, and Ended
-    if status == "Ended"
-      status = %w[Won Lost Ended]
-      current_user.auctions.each do |auction|
-        if status.include? auction.auction_status
-          @auctions.push auction
-        end
-      end
-    # If there was no status parameter for some reason, just display all
-    elsif status == nil
-      return current_user.auctions
-    # Else, just match the status
-    else
-      current_user.auctions.each do |auction|
-        if auction.auction_status == status.to_s
-          @auctions.push auction
-        end
-      end
-    end
-    @auctions
-  end
-  
-  # Extracts the item_id from the URL if the entry is not only digits. Otherwise, the entry is just returned.
-  def parse_url_for_item_id(url)
-    if url.match(/\D*/).to_s.length != 0
-      @item_id = url.match(/item=\d*\D/).to_s.gsub!(/\D+/, "")
-    else
-      url
-    end
-  end
-  
-  # Calculates the time remaining on the auction minus 2 minutes
-  def get_enqueue_time(auction_end_time, lead_time)
-    auction_end_time = Time.parse(auction_end_time).localtime
-    return auction_end_time - Time.now - 120
   end
 end
--- app/models/auction.rb
+++ app/models/auction.rb
@@ -1,10 +1,17 @@
 class Auction < ActiveRecord::Base
   attr_accessible :item_id, :max_bid, :user_id, :item, :picture, :user_notification, :id
   belongs_to :user
-  validates_uniqueness_of :item_id, :scope => :user_id, :message => "This auction has already been added."
-  validates_presence_of :max_bid, :message => "You must enter a max bid."
+  validates_uniqueness_of :item_id, :scope => :user_id, :message => "has already been added."
+  validates_presence_of :max_bid, :message => "must be entered."
+  validate :user_has_phone_if_notify
   
   serialize :picture, Array
+  
+  def user_has_phone_if_notify
+    if user_notification != "Do not notify" && user.phone_number == ""
+      errors.add :user_notification, "requires that you provide a phone number under the \"Edit account\" page."
+    end
+  end
   
   def item=(value)
     super(Marshal.dump(value))
@@ -15,6 +22,117 @@ class Auction < ActiveRecord::Base
       Marshal.load(super)
     else
       nil
+    end
+  end
+  
+  def prepare(auction, current_user)
+    auction.user = current_user
+    
+    # Parse the eBay item URL for the item's ID, then get the item's info
+    auction.item_id = parse_url_for_item_id(auction.item_id)
+    auction.item = EbayAction.new.get_item(auction.item_id, "")
+    
+    # If the auction is real
+    if auction.item[:get_item_response][:ack] == "Success"
+      find_status(auction)
+    
+      # Load the listing's pictures. If the item's seller didn't include a picture, load ebay's
+      # default picture. Else, check if there are multiple pictures. If true, push them all into the
+      # pictures array. If there's only one picture, then push that in.
+      if auction.item[:get_item_response][:item][:picture_details][:photo_display] == "None"
+        auction.picture.push "http://p.ebaystatic.com/aw/pics/nextGenVit/imgNoImg.gif"
+      else
+        @pictures = auction.item[:get_item_response][:item][:picture_details][:picture_url]
+        if @pictures.respond_to?(:each)
+          @pictures.each do |picture|
+            auction.picture.push picture.to_s
+          end
+        else
+          auction.picture.push @pictures.to_s
+        end
+      end
+    
+      # If the auction is still going, enqueue an AuctionBidder worker to bid on the auction
+      if auction.auction_status == "Active"
+        Resque.enqueue_at(get_enqueue_time(auction.item[:get_item_response][:item][:listing_details][:end_time],
+                          auction.lead_time).seconds.from_now, AuctionBidder, auction.id)
+      end
+    # The auction does not exist. Redirect to new auction page.
+    else
+      #redirect_to new_auction_path, notice: "The auction's item ID was invalid."
+      auction.item = EbayAction.new.get_item("110101276115", "")
+    end
+  end
+
+  def update_auction(auction)
+    if auction.auction_status == "Active"
+      # Sadly, this doesn't work because of the nested hashes.
+      # auction.item.merge! EbayAction.new.get_item(auction.item_id, "timeleft,bidcount,currentprice,userid")
+      
+      # Figure out how to update while only grabbing values needed
+      @new_auction = EbayAction.new.get_item(auction.item_id, "")
+      auction.item = auction.item.merge @new_auction
+      find_status(auction)
+      auction.save
+    end
+  end
+  
+  # Returns the appropriate auctions based on the user's selected auction status preference.
+  def self.sort_auctions(status, current_user)
+    @auctions = []
+    # If the status == "Ended" return all Won, Lost, and Ended
+    if status == "Ended"
+      status = %w[Won Lost Ended]
+      current_user.auctions.each do |auction|
+        if status.include? auction.auction_status
+          @auctions.push auction
+        end
+      end
+    # If there was no status parameter for some reason, just display all
+    elsif status == nil
+      return current_user.auctions
+    # Else, just match the status
+    else
+      current_user.auctions.each do |auction|
+        if auction.auction_status == status.to_s
+          @auctions.push auction
+        end
+      end
+    end
+    @auctions
+  end
+  
+  # Calculates the time remaining on the auction minus 2 minutes
+  def get_enqueue_time(auction_end_time, lead_time)
+    auction_end_time = Time.parse(auction_end_time).localtime
+    return auction_end_time - Time.now - 120
+  end
+  
+  # Extracts the item_id from the URL if the entry is not only digits. Otherwise, the entry is just returned.
+  def parse_url_for_item_id(url)
+    if url.match(/\D*/).to_s.length != 0
+      @item_id = url.match(/item=\d*\D/).to_s.gsub!(/\D+/, "")
+    else
+      url
+    end
+  end
+  
+  # Finds the current status of the auction (active, won, lost, etc)
+  def find_status(auction)
+    # If the auction is over, check if we won or lost
+    if auction.item[:get_item_response][:item][:time_left] == "PT0S"
+      # Change current_user.name to wherever the user's ebay username is stored
+      begin
+        if auction.item[:get_item_response][:item][:selling_status][:high_bidder][:user_id] == "testuser_jpwendt2"
+          auction.auction_status = "Won"
+        else
+          auction.auction_status = "Lost"
+        end
+      rescue
+        auction.auction_status = "Lost"
+      end
+    else
+      auction.auction_status = "Active"
     end
   end
   
